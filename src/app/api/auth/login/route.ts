4a294cdeaf7645bd73cd323b56777705ba214e27
Paulo Victor <paulovmmf@gmail.com>
2026-05-02 21:33:51 -0300

    Add ShadCN e Modificando caminhos

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-
 const prisma = new PrismaClient()
 
 export async function POST(req: Request) {
